8509973b3c724372b9256f7320c0a2186efc361c
Amine Damir <contact@aminedamir.com>
2025-02-15 20:56:40 +0100

    Initial commit for retell-client-js-sdk static hosting

---
+++ browser-sdk.js
@@ -0,0 +1,4 @@
+// Import the Retell client SDK for browsers
+import { RetellWebClient } from 'retell-client-js-sdk';
+// Expose the Retell Web Client globally so it can be accessed as window.RetellWebClient
+window.RetellWebClient = RetellWebClient;
